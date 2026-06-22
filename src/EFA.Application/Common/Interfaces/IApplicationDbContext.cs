dd1e4a944ebd49d0b24c0df3fa301ae9b88bdeb6
Your Name <mseif1719@gmail.com>
2026-06-22 12:55:39 +0300

    add tables to database

---
+++ src/EFA.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFA.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/EFA.Infrastructure/Persistence/DbInitializer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFA.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFA.Infrastructure.Persistence
+{
+    public static class DbInitializer
+    {
+        public static async Task SeedAsync(
+            IServiceProvider serviceProvider)
+        {
+            var roleManager =
+                serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var userManager =
+                serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            string[] roles =
+            [
+                "Admin",
+            "CompetitionsAdmin",
+            "RefereeCommittee",
+            "SecurityOfficer",
+            "ReportsViewer"
+            ];
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(
+                        new IdentityRole(role));
+                }
+            }
+
+            var adminEmail = "admin@efa.local";
+
+            var admin =
+                await userManager.FindByEmailAsync(adminEmail);
+
+            if (admin is null)
+            {
+                admin = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FullName = "System Administrator",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(
+                    admin,
+                    "Admin@123");
+
+                await userManager.AddToRoleAsync(
+                    admin,
+                    "Admin");
+            }
+        }
+    }
+}
